0f7500b55313d9b0d2359ae329798a90b753e0ed
Ricky <rzh4321@gmail.com>
2023-05-21 23:40:45 -0400

    added bg color to "read" cells, can now be toggled

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function checkValidity() {
     return title.value != '' && author.value != '' && pages.value != '' && desc.value != '';
 }
 
+function switchRead(elem) {
+    if (elem.classList.contains('on')) {
+        elem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+      </svg>`; 
+    }
+    else {
+        elem.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5' /></svg>"
+    }
+    elem.classList.toggle('on')
+}
+
 function add_book(e) {
     let table = document.getElementById('table');
     let row = document.createElement('tr');
@@ -24,16 +36,20 @@ function add_book(e) {
         td.textContent = val.value;
         row.append(td);
     }
+    
+    let td2 = document.createElement('td');
+    td2.id = 'read-cell';
     if (read.checked) {
-        let td2 = document.createElement('td');
-        td2.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5' /></svg>"
-        row.append(td2);
+        td2.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'><path stroke-linecap='round' stroke-linejoin='round' d='M4.5 12.75l6 6 9-13.5' /></svg>" 
+        td2.classList.add('on');
     }
     else {
-        let td2 = document.createElement('td');
         td2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>`; 
-      row.append(td2);
     }
+    td2.addEventListener('click', function(e) {
+        switchRead(td2);
+    });
+    row.append(td2);
 }
