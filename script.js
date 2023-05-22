0ed07441de6a1a8fff042792deb3c2f383433bd2
Ricky <rzh4321@gmail.com>
2023-05-21 22:30:45 -0400

    added form validation and button updates table

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let add_button = document.getElementById('add-book');
-add_button.addEventListener('click', add_book);
+add_button.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (checkValidity()) {
+        add_book();
+    }
+});
+alert(read.value);
 
-function add_book() {
+function checkValidity() {
+    const arr = [title, author, pages, desc];
+    for (const val of arr) {
+        if (val.value == '') val.classList.add('invalid');
+        else val.classList.remove('invalid');
+    }
+    return title.value != '' && author.value != '' && pages.value != '' && desc.value != '';
+}
+
+function add_book(e) {
+    let table = document.getElementById('table');
+    let row = document.createElement('tr');
+    table.append(row);
+    const arr = [title, author, desc, pages];
+    for (const val of arr) {
+        let td = document.createElement('td');
+        td.textContent = val.value;
+        row.append(td);
+    }
     
 }
